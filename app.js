2e29455eae921e7721606fe25a94d36348e238e8
Andrew <andrewchau1993@gmail.com>
2024-12-30 15:57:50 -0800

    added in the top columns

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ function preload() {
 // function to generate elements while will appear in the game
 function create() {
   const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+  const roads = this.physics.add.staticGroup();
+  const road = roads.create(400,568, 'road').setScale(2).refreshBody()
+  const topColumns = this.physics.add.staticGroup({
+    key: 'column',
+    repeat: 1,
+    setXY: { x:200, y:0, stepX:300 }
+  })
 }
 //function used for updating the bird
 function update(){
