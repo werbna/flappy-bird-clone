4e112bf83e9aac23d449e36dcb25e9dd27a144dc
Andrew <andrewchau1993@gmail.com>
2024-12-30 15:42:13 -0800

    added inital game configuration

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+//game configuration
+let config = {
+  renderer: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+// function to bring in images for our application, such as the background.
 function preload() {
 
 }
-
+// function to generate elements while will appear in the game
 function create() {
 
 }
+//function used for updating the bird
 function update(){
 
 }
