bd656710e367ffaa7cea22adccf8632ff6e54739
Andrew <andrewchau1993@gmail.com>
2024-12-30 16:03:21 -0800

    added in bird and collision logic

--- app.js
+++ app.js
@@ -4,40 +4,52 @@ let config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       gravity: { y: 300 },
-      debug: false
-    }
+      debug: false,
+    },
   },
   scene: {
     preload: preload,
     create: create,
-    update: update
-  }
+    update: update,
+  },
 };
 
-let game = new Phaser.Game(config)
+let game = new Phaser.Game(config);
 
 // function to bring in images for our application, such as the background.
 function preload() {
-  this.load.image('background', 'assets/background.png');
-  this.load.image('road', 'assets/road.png');
-  this.load.image('column', 'assets/column.png');
-  this.load.spritesheet('bird', 'assets/bird.png', { frameWidth: 64, frameHeight: 96 });
+  this.load.image("background", "assets/background.png");
+  this.load.image("road", "assets/road.png");
+  this.load.image("column", "assets/column.png");
+  this.load.spritesheet("bird", "assets/bird.png", {
+    frameWidth: 64,
+    frameHeight: 96,
+  });
 }
+
+let bird;
 // function to generate elements while will appear in the game
 function create() {
-  const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+  const background = this.add.image(0, 0, "background").setOrigin(0, 0);
   const roads = this.physics.add.staticGroup();
-  const road = roads.create(400,568, 'road').setScale(2).refreshBody()
+  const road = roads.create(400, 568, "road").setScale(2).refreshBody();
   const topColumns = this.physics.add.staticGroup({
-    key: 'column',
+    key: "column",
     repeat: 1,
-    setXY: { x:200, y:0, stepX:300 }
-  })
+    setXY: { x: 200, y: 0, stepX: 300 },
+  });
+  const bottomColumns = this.physics.add.staticGroup({
+    key: "column",
+    repeat: 1,
+    setXY: { x: 350, y: 400, stepX: 300 },
+  });
+  bird = this.physics.add.sprite(0,50,'bird').setScale(2);
+  bird.setBounce(.2);
+  bird.setCollideWorldBounds(true)
+  this.physics.add.collider(bird, road);
 }
 //function used for updating the bird
-function update(){
-
-}
+function update() {}
